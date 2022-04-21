a8dcf406f6b098ffb31b2d5daed353d587a9d891
carru <carru400@gmail.com>
2022-04-20 21:22:53 -0700

    add note live preview

--- src/notes/NoteEditor.tsx
+++ src/notes/NoteEditor.tsx
@@ -1,20 +1,49 @@
-import { FC } from "react";
+import { addDoc, CollectionReference, Timestamp, updateDoc } from "firebase/firestore";
+import { FC, useState } from "react";
 import { FormControl } from "react-bootstrap";
 import { Note } from "./Note";
+import { marked  } from "marked";
+import DOMPurify from "dompurify";
 
 interface NoteEditorProps {
     activeNote: Note,
-    onContentChanged: (newContent: string) => void
+    notesRef: CollectionReference<Note>,
+    setActiveNote: React.Dispatch<React.SetStateAction<Note | undefined>>
 }
 
 export const NoteEditor: FC<NoteEditorProps> = (props) => {
+    const [htmlPreview, setHtmlPreview] = useState('');
+    
+    const onContentChanged = async (newContent: string) => {
+        if (!props.activeNote) return;
+
+        const newNote = {...props.activeNote};
+        newNote.content = newContent;
+        props.setActiveNote(newNote);
+
+        // Update note on firestore
+        if (newNote.ref) {
+            await updateDoc(newNote.ref, {
+                content: newContent,
+                lastModified: Timestamp.now()
+            });
+        } else {
+            await addDoc(props.notesRef, newNote);
+        }
+
+        // Update html preview
+        const rawHtml = marked.parse(newContent);
+        setHtmlPreview(DOMPurify.sanitize(rawHtml, { USE_PROFILES: { html: true } }));
+    };
+
     return (
         <>
             <FormControl
                 as="textarea"
-                onChange={(e) => props.onContentChanged(e.target.value)}
+                onChange={(e) => onContentChanged(e.target.value)}
                 value={props.activeNote.content}
             />
+            <div dangerouslySetInnerHTML={{ __html: htmlPreview }} />
         </>
     );
 }
--- src/notes/NotesView.tsx
+++ src/notes/NotesView.tsx
@@ -1,5 +1,5 @@
 import { User } from "firebase/auth";
-import { addDoc, collection, getFirestore, Timestamp, updateDoc } from "firebase/firestore";
+import { collection, getFirestore } from "firebase/firestore";
 import { FC, useState } from "react";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import { app } from "../firebase";
@@ -17,28 +17,10 @@ export const NotesView: FC<NotesViewProps> = (props) => {
     const notesRef = collection(getFirestore(app), 'users', props.user.uid, 'notes').withConverter(converter);
     const [notes] = useCollectionData(notesRef);
 
-    const onContentChanged = async (newContent: string) => {
-        if (!activeNote) return;
-
-        const newNote = {...activeNote};
-        newNote.content = newContent;
-        setActiveNote(newNote);
-
-        // Update note on firestore
-        if (newNote.ref) {
-            await updateDoc(newNote.ref, {
-                content: newContent,
-                lastModified: Timestamp.now()
-            });
-        } else {
-            await addDoc(notesRef, newNote);
-        }
-    };
-
     return (
         <>
             <NotesList user={props.user} activeNote={activeNote} setActiveNote={setActiveNote} notes={notes}/>
-            {activeNote && <NoteEditor activeNote={activeNote} onContentChanged={onContentChanged} />}
+            {activeNote && <NoteEditor activeNote={activeNote} notesRef={notesRef} setActiveNote={setActiveNote} />}
         </>
     );
 }
